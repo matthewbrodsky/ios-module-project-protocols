24d376354939c620da66109bec6c6bd90d6edf7b
Matthew Brodsky <matthewbrodsky1265@me.com>
2020-03-18 18:28:19 -0400

    First commit only got to step 4

--- CardGame.playground/Contents.swift
+++ CardGame.playground/Contents.swift
@@ -2,26 +2,66 @@ import Foundation
 
 //: ## Step 1
 //: Create an enumeration for the value of a playing card. The values are: `ace`, `two`, `three`, `four`, `five`, `six`, `seven`, `eight`, `nine`, `ten`, `jack`, `queen`, and `king`. Set the raw type of the enum to `Int` and assign the ace a value of `1`.
-
+enum Rank: Int {
+    case ace = 1
+    case two = 2
+    case three = 3
+    case four = 4
+    case five = 5
+    case six = 6
+    case seven = 7
+    case eight = 8
+    case nine = 9
+    case ten = 10
+    case jack = 11
+    case queen = 12
+    case king = 13
+}
 
 
 
 
 //: ## Step 2
 //: Once you've defined the enum as described above, take a look at this built-in protocol, [CustomStringConvertible](https://developer.apple.com/documentation/swift/customstringconvertible) and make the enum conform to that protocol. Make the face cards return a string of their name, and for the numbered cards, simply have it return that number as a string.
+extension Rank: CustomStringConvertible {
+    var description: String {
+        switch self {
+            case .jack:
+                return "Jack"
+            case .queen:
+                return "Queen"
+            case .king:
+                return "King"
+            case .ace:
+                return "Ace"
+            default:
+            return "\(rawValue)"
+        }
+
+    }
+
+}
 
 
 
 
 //: ## Step 3
 //: Create an enum for the suit of a playing card. The values are `hearts`, `diamonds`, `spades`, and `clubs`. Use a raw type of `String` for this enum (this will allow us to get a string version of the enum cases for free, no use of `CustomStringConvertible` required).
-
+enum Suit: String {
+    case heart = "hearts"
+    case diamond = "diamonds"
+    case spade = "spades"
+    case club = "clubs"
+}
 
 
 
 //: ## Step 4
 //: Using the two enums above, create a `struct` called `Card` to model a single playing card. It should have constant properties for each constituent piece (one for suit and one for rank).
-
+struct Card {
+    let rank: Rank
+    let suit: Suit
+}
 
 
 
@@ -32,7 +72,9 @@ import Foundation
 
 //: ## Step 6
 //: Create a `struct` to model a deck of cards. It should be called `Deck` and have an array of `Card` objects as a constant property. A custom `init` function should be created that initializes the array with a card of each rank and suit. You'll want to iterate over all ranks, and then over all suits (this is an example of _nested `for` loops_). See the next 2 steps before you continue with the nested loops.
-
+struct Deck {
+    let cards: [Card]
+}
 
 
 
